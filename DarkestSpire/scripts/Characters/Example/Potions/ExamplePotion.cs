134971484f3cd8a8ea56c0aef173c8823b4afbba
JIMJMjm <emmmem3@163.com>
2026-08-09 16:14:00 +0800

    260809: Added auto pack after `Publish` event & More example items

---
+++ DarkestSpire/scripts/Characters/Example/Potions/ExamplePotion.cs
@@ -0,0 +1,44 @@
+﻿using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Potions;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.Models.PotionPools;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace DarkestSpire.Characters.Example.Potions;
+
+// 注册药水。如果要写自定义池看添加人物的开头
+[RegisterPotion(typeof(SharedPotionPool))]
+public class TestPotion : ModPotionTemplate
+{
+    // 稀有度
+    public override PotionRarity Rarity => PotionRarity.Common;
+
+    // 使用方式，CombatOnly表示只能在战斗中使用。
+    public override PotionUsage Usage => PotionUsage.CombatOnly;
+
+    // 目标类型
+    public override TargetType TargetType => TargetType.Self;
+
+    // 定义动态变量
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(3)];
+
+    // 这里显示预览卡牌灵魂。或者你也可以添加提示关键词
+    protected override IEnumerable<IHoverTip> AdditionalHoverTips => [HoverTipFactory.FromCard<Soul>()];
+
+    // 药水图片。不一定非得是png，只要最终能被Godot当成Texture读取即可。
+    public override PotionAssetProfile AssetProfile => new(
+        ImagePath: "res://DarkestSpire/images/potions/{GetType().Name}.png",
+        OutlinePath: "res://DarkestSpire/images/potions/{GetType().Name}.png"
+    );
+
+    // 使用时的效果逻辑，这里是创造3张灵魂到手牌中。
+    protected override async Task OnUse(PlayerChoiceContext choiceContext, Creature? target)
+    {
+        await Soul.CreateInHand(Owner, DynamicVars.Cards.IntValue, Owner.Creature.CombatState!);
+    }
+}

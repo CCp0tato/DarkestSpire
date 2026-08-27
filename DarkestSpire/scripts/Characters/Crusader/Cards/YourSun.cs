ba40e7ab44c3ef4f94f57649ed6d3af6550bc852
lycshanshan <lycshanshan@gmail.com>
2026-08-27 14:19:56 +0800

    260827: Add Cards

--- DarkestSpire/scripts/Characters/Crusader/Cards/YourSun.cs
+++ DarkestSpire/scripts/Characters/Crusader/Cards/YourSun.cs
@@ -1,4 +1,9 @@
+// | 你的太阳…… | YourSun | 能力 | 0 | 回合开始时，将一张（—/升级后的）濡湿小镰刀加入你的抽牌堆 |
+
+using DarkestSpire.GeneralPowers;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using STS2RitsuLib.Interop.AutoRegistration;
 using STS2RitsuLib.Scaffolding.Content;
 
@@ -11,6 +16,17 @@ public class YourSun : ModCardTemplate
     {
     }
 
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (IsUpgraded)
+        {
+            await PowerCmd.Apply<YourSunUpgradePower>(choiceContext, Owner.Creature, 1, Owner.Creature, this);
+        }
+        else
+        {
+            await PowerCmd.Apply<YourSunPower>(choiceContext, Owner.Creature, 1, Owner.Creature, this);
+        }
+    }
 
     // 卡图资源
     public override CardAssetProfile AssetProfile => new(
